1f979015fe7c169bd719a00219cb6250db0a5bd0
jfarrell <jfarrell@g.harvard.edu>
2018-03-10 13:01:18 -0500

    sva now only required if batchCombat is run.

--- R/batch.R
+++ R/batch.R
@@ -1,7 +1,5 @@
 #' Batch correct data using ComBat
 #' 
-#' @importFrom sva ComBat
-#' 
 #' @param object An URD object
 #' @param batch.column (Character) Name of column in \code{@@meta} that encodes batch information.
 #' @param adjustment.columns (Character vector) Name of columns in \code{@@meta} that encode information to make sure not to correct (e.g. developmental stage, etc.)
@@ -18,42 +16,46 @@
 #'
 #' @export
 batchComBat <- function(object, batch.column, adjustment.columns=NULL, reference.batch=NULL, genes.correct=NULL, thresh.out=.05, min.out=0, max.out=(ceiling(max(object@logupx.data))+1), parametric.prior=T, prior.plots=F, verbose=F) {
-  
-  # Build model matrix if adjustment columns exist
-  if (verbose) print(paste0(Sys.time(), ": Building model matrix."))
-  if (!is.null(adjustment.columns)) {
-    matrix.call <- paste0("model.matrix(~", paste(adjustment.columns, collapse="+"), ", data=object@meta)")
-    model.matrix <- eval(parse(text=matrix.call))
+  # Bioconductor package 'sva' is required for ComBat batch correction.
+  if (requireNamespace("sva", quietly=T)) {
+    
+    # Build model matrix if adjustment columns exist
+    if (verbose) print(paste0(Sys.time(), ": Building model matrix."))
+    if (!is.null(adjustment.columns)) {
+      matrix.call <- paste0("model.matrix(~", paste(adjustment.columns, collapse="+"), ", data=object@meta)")
+      model.matrix <- eval(parse(text=matrix.call))
+    } else {
+      model.matrix <- NULL
+    }
+    
+    # Get genes.correct
+    if (is.null(genes.correct)) genes.correct <- rownames(object@logupx.data)
+    
+    # Filter genes to those with variance > 0
+    if (verbose) print(paste0(Sys.time(), ": Removing genes with no variance."))
+    if (is.null(reference.batch)) {
+      var <- apply(object@logupx.data[genes.correct,], 1, var)
+      genes.use <- names(which(var>0))
+    } else {
+      ref.cells <- rownames(object@meta)[which(object@meta[,batch.column] == reference.batch)]
+      var <- apply(object@logupx.data[genes.correct,ref.cells], 1, var)
+      genes.use <- names(which(var>0))
+    }
+    if (verbose) print(paste("Retaining", length(genes.use), "of", nrow(object@logupx.data), "genes."))
+    
+    # Run ComBat
+    if (verbose) print(paste0(Sys.time(), ": Starting ComBat."))
+    batch.data <- as.matrix(ComBat(dat=as.matrix(object@logupx.data[genes.use,]), batch=object@meta[colnames(object@logupx.data), batch.column], mod=model.matrix, prior.plots=prior.plots, par.prior=parametric.prior, ref.batch=reference.batch))
+    
+    # Crop extreme values occasionally produced by Combat
+    if (verbose) print(paste0(Sys.time(), ": Correcting extreme values and re-sparsifying matrix."))
+    batch.data[batch.data < thresh.out] <- min.out
+    batch.data[batch.data > max.out] <- max.out
+    
+    # Replace logupx data with batch-corrected data.
+    object@logupx.data <- as(round(batch.data, digits=4), "dgCMatrix")
+    return(object)
   } else {
-    model.matrix <- NULL
+    stop("Package sva from bioConductor is required for this function. To install:\nsource('https://bioconductor.org/biocLite.R')\nbiocLite('sva')\n")
   }
-  
-  # Get genes.correct
-  if (is.null(genes.correct)) genes.correct <- rownames(object@logupx.data)
-  
-  # Filter genes to those with variance > 0
-  if (verbose) print(paste0(Sys.time(), ": Removing genes with no variance."))
-  if (is.null(reference.batch)) {
-    var <- apply(object@logupx.data[genes.correct,], 1, var)
-    genes.use <- names(which(var>0))
-  } else {
-    ref.cells <- rownames(object@meta)[which(object@meta[,batch.column] == reference.batch)]
-    var <- apply(object@logupx.data[genes.correct,ref.cells], 1, var)
-    genes.use <- names(which(var>0))
-  }
-  if (verbose) print(paste("Retaining", length(genes.use), "of", nrow(object@logupx.data), "genes."))
-  
-  # Run ComBat
-  if (verbose) print(paste0(Sys.time(), ": Starting ComBat."))
-  batch.data <- as.matrix(ComBat(dat=as.matrix(object@logupx.data[genes.use,]), batch=object@meta[colnames(object@logupx.data), batch.column], mod=model.matrix, prior.plots=prior.plots, par.prior=parametric.prior, ref.batch=reference.batch))
-  #batch.data <- as.matrix(combat.original(dat=as.matrix(object@logupx.data[genes.use,]), batch=object@meta[colnames(object@logupx.data), batch.column], mod=model.matrix, prior.plots=prior.plots, par.prior=parametric.prior, ref.batch=reference.batch))
-  
-  # Crop extreme values occasionally produced by Combat
-  if (verbose) print(paste0(Sys.time(), ": Correcting extreme values and re-sparsifying matrix."))
-  batch.data[batch.data < thresh.out] <- min.out
-  batch.data[batch.data > max.out] <- max.out
-  
-  # Replace logupx data with batch-corrected data.
-  object@logupx.data <- as(round(batch.data, digits=4), "dgCMatrix")
-  return(object)
 }
