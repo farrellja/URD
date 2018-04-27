89ab28b7db61bdddcc4ed1e1594338b27e014eaf
jfarrell <jfarrell@g.harvard.edu>
2018-04-26 21:55:18 -0400

    Updating Installation Instructions

--- URD-Install.R
+++ URD-Install.R
@@ -2,10 +2,6 @@ message("Loading Bioconductor to install required packages.")
 source("https://bioconductor.org/biocLite.R")
 message("Installing required packages from Bioconductor.")
 biocLite(c('Biobase', 'S4Vectors', 'AnnotationDbi', 'destiny'), suppressUpdates=T)
-if (!requireNamespace("ROCR", quietly = TRUE)) {
-  message("Installing ROCR")
-  install.packages("ROCR")
-}
 if (requireNamespace("devtools", quietly = TRUE)) {
   message("Installing URD")
   devtools::install_github(repo="farrellja/URD")
