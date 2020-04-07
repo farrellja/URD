f76016f3d2e4ce678976b319a7378f4f8ba5d14d
farrellja <jeffrey.farrell@nih.gov>
2020-04-07 17:25:10 -0400

    Check no tips are specified without cells from loadTipCells.

--- R/tree.R
+++ R/tree.R
@@ -88,6 +88,10 @@ buildTree <- function(object, pseudotime, tips.use=NULL, divergence.method=c("ks
   if (weighted.fusion) {
     tip.size <- unlist(lapply(object@tree$cells.in.tip, length))
     if (length(tip.size)==0) stop("Either weighted.fusion must be false, or loadTipCells must be run prior.")
+    tip.size <- tip.size[tip.size > 0]
+    if !all(tips.use %in% names(tip.size)) {
+      stop(paste0("tips.use included tips (", paste0(setdiff(tips.use, names(tip.size)), collapse=", "), ") for which no cells were defined by loadTipCells. Does that tip exist in your tip clustering?"))
+    }
   }
   # Load the pseudotime used into the tree object for use later with plotting
   object@tree$pseudotime <- object@pseudotime[,pseudotime]
